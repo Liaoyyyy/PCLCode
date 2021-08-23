a7b31d6174375287199bf26f021d4fe7fab1eacb
慕竹 <liao.yanjun@outlook.com>
2021-08-23 22:32:56 +0800

    今天完成了点云的载入，显示两个内容，明天再试试分割，滤波等操作

--- Test1_readPly/readPly.cpp
+++ Test1_readPly/readPly.cpp
@@ -1,70 +1,52 @@
-#include <pcl/visualization/cloud_viewer.h>
-#include <iostream>//标准C++库中的输入输出类相关头文件。
-#include <pcl/io/io.h>
-#include <pcl/io/pcd_io.h>//pcd 读写类相关的头文件。
-#include <pcl/io/ply_io.h>
-#include <pcl/point_types.h> //PCL中支持的点类型头文件。
-#include<fstream>  
-#include <string>  
-#include <vector> 
+#include <iostream> //标准输入输出流
+#include <pcl/io/pcd_io.h>//PCL的PCD格式文件的输入输出头文件
+#include <pcl/io/ply_io.h>//PCL的PLY格式文件的输入输出头文件
+#include <pcl/point_types.h>//PCL中支持的点类型文件
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>//可视化头文件
 
-using namespace std;
-
-int main()
-{
-	typedef struct tagPOINT_3D
-	{
-		double x;  //mm world coordinate x  
-		double y;  //mm world coordinate y  
-		double z;  //mm world coordinate z  
-		double r;
-	}POINT_WORLD;
-
-
-	// 加载txt数据
-	int number_Txt;
-	FILE *fp_txt;
-	tagPOINT_3D TxtPoint;
-	vector<tagPOINT_3D> m_vTxtPoints;
-	fp_txt = fopen("pcd.txt", "r");
-	if (fp_txt)
-	{
-		while (fscanf(fp_txt, "%lf %lf %lf", &TxtPoint.x, &TxtPoint.y, &TxtPoint.z) != EOF)
-		{
-			m_vTxtPoints.push_back(TxtPoint);
-		}
+//对输入点云的过程进行一个包装。
+bool myLoadPly(const std::string fileName, pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud) {
+	if ((pcl::io::loadPLYFile<pcl::PointXYZRGB>(fileName, *cloud) < 0)) {
+		std::cout << "文件读取失败！" << std::endl;
+		return false;
 	}
-	else
-		cout << "txt数据加载失败！" << endl;
-	number_Txt = m_vTxtPoints.size();
-	//pcl::PointCloud<pcl::PointXYZ> cloud;
-	//这里使用“PointXYZ”是因为我后面给的点云信息是包含的三维坐标，同时还有点云信息包含的rgb颜色信息的或者还有包含rgba颜色和强度信息。
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-	// Fill in the cloud data  
-	cloud->width = number_Txt;
-	cloud->height = 1;
-	cloud->is_dense = false;
-	cloud->points.resize(cloud->width * cloud->height);
-	for (size_t i = 0; i < cloud->points.size(); ++i)
+	else {
+		std::cout << "文件读取成功！" << std::endl;
+		return false;
+
+	};
+}
+//对点云进行可视化
+bool myViewer(const std::string dataName, pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud) {
+	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer(dataName));
+	//背景色
+	viewer->setBackgroundColor(0, 0, 0);
+	//数据
+	viewer->addPointCloud<pcl::PointXYZRGB>(cloud, dataName);
+	//点大小
+	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, dataName);
+	//添加坐标系（即红绿蓝三色轴，放置在原点）
+	//viewer->addCoordinateSystem(3.0);//3.0指轴的长度
+	//viewer->addCoordinateSystem (3.0,1,2,3);一个重载函数，3.0指轴的长度，放置在（1，2，3）位置
+	//初始化默认相机参数
+	viewer->initCameraParameters();
+	while (!viewer->wasStopped())
 	{
-		cloud->points[i].x = m_vTxtPoints[i].x;
-		cloud->points[i].y = m_vTxtPoints[i].y;
-		cloud->points[i].z = m_vTxtPoints[i].z;
+		viewer->spinOnce();
+		
 	}
-	pcl::io::savePCDFileASCII("txt2pcd_bunny1.pcd", *cloud);
-	std::cerr << "Saved " << cloud->points.size() << " data points to txt2pcd.pcd." << std::endl;
+	return true;
+}
+int main() {
 
-	//for (size_t i = 0; i < cloud.points.size(); ++i)
-	//  std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
-
-	//PCL Visualizer
-	// Viewer  
-	pcl::visualization::PCLVisualizer viewer("Cloud Viewer");
-	viewer.addPointCloud(cloud);
-	viewer.setBackgroundColor(0, 0, 0);
-
-	viewer.spin();
+	//创建共享指针并实例化指针
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new  pcl::PointCloud<pcl::PointXYZRGB>);
+	std::string fileName = "longdress_vox10_1051.ply";
+	//读取ply文件
+	bool isload = myLoadPly(fileName, cloud);
+	//可视化点云
+	//bool isView = myViewer("longDress_1051", cloud);
 	system("pause");
 	return 0;
-
 }
